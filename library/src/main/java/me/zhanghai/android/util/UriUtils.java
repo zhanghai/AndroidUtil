0456f1b35625b2936e1d1f376503f9c0d1eed1d4
Zhang Hai <dreaming.in.code.zh@gmail.com>
2015-06-06 22:55:41 +0800

    Fix UriUtils.

--- library/src/main/java/me/zhanghai/android/util/UriUtils.java
+++ library/src/main/java/me/zhanghai/android/util/UriUtils.java
@@ -5,11 +5,13 @@
 
 package me.zhanghai.android.util;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.OpenableColumns;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
@@ -24,9 +26,20 @@ public class UriUtils {
         }
     }
 
+    // See https://developer.android.com/training/secure-file-sharing/retrieve-info.html#RetrieveFileInfo
     public static OpenableInfo queryOpenableInfo(Uri uri, Context context) {
+
         Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
+
+        if (cursor == null || !cursor.moveToFirst()) {
+            // Fallback: Check if it is a file Uri.
+            if (uri.getScheme().equals(ContentResolver.SCHEME_FILE)) {
+                File file = new File(uri.getPath());
+                return new OpenableInfo(file.getName(), file.length());
+            }
+            return null;
+        }
+
         OpenableInfo info = new OpenableInfo(
                 cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)),
                 cursor.getLong(cursor.getColumnIndex(OpenableColumns.SIZE)));
